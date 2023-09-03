ec8e3280b6b66575d6bdd9e8e3ffe3dc6b013a44
malawad <50057882+malawadd@users.noreply.github.com>
2023-09-03 22:51:16 +0300

    🚣 drop down menue 🚣

---
+++ webapp/src/app/components/Dropdown/constants.tsx
@@ -0,0 +1,27 @@
+import { EthereumLogo, BNBLogo, TokenLogo } from "../../../assets/images/imageAssets";
+
+export const currencyList = [
+    {
+        value: "eth",
+        label: "ETH",
+        icon: EthereumLogo,
+    },
+    {
+        value: "bnb",
+        label: "BNB",
+        icon: BNBLogo,
+    },
+];
+
+export const tokenList = [
+    {
+        value: "mun",
+        label: "MUN",
+        icon: TokenLogo,
+    },
+    {
+        value: "nav",
+        label: "NAV",
+        icon: TokenLogo,
+    },
+];
